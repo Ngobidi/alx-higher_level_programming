6765eb89dfa770710514ed6e9ca7452a19e39f1a
Ngobidi <chibuzormaxwell@mail.com>
2023-10-05 07:37:40 -0700

    updates

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-argc = len(argv)
-if argc < 3:
-    print("{} arguments.".format(argc - 2))
-else:
-    if argc == 3:
-        print("{} argument:".format(argc - 2))
-    else:
-        print("{} arguments:".format(argc - 2))
+    z = len(argv) - 1
+    if(z == 0):
+        print("0 arguments.")
