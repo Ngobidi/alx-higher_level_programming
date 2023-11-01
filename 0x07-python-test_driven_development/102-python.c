a55fe5609771e57aa42437f155cce6e94c08f728
Ngobidi <chibuzormaxwell@mail.com>
2023-11-01 02:22:30 -0700

    updated

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -6,23 +6,16 @@
  */
 void print_python_string(PyObject *p)
 {
-	long int len;
-
-	fflush(stdout);
-
-	printf("[.] string object info\n");
-	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	wprintf(L"[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str"))
 	{
-		printf("  [ERROR] Invalid String Object\n");
+		wprintf(L"  [ERROR] Invalid String Object\n");
 		return;
 	}
-
-	len = ((PyASCIIObject *)(p))->len;
-
 	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii\n");
+		wprintf(L"  type: compact ascii\n");
 	else
-		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", len);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &len));
+		wprintf(L"  type: compact unicode object\n");
+	wprintf(L"  length: %lu\n", PyUnicode_GET_LENGTH(p));
+	wprintf(L"  value: %ls\n", PyUnicode_AS_UNICODE(p));
 }
