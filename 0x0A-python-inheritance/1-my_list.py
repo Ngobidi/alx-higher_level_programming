ebe6c0cf0dedf4eac188a0b4bcd14132c47d64d8
Ngobidi <chibuzormaxwell@mail.com>
2023-11-06 06:01:02 -0800

    updated

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,7 +5,9 @@
 class MyList(list):
     """Mylist class that inherites from list"""
     def print_sorted(self):
-        """function that prints the list, but sorted"""
-        sorted_list = sorted(self)
-        print(sorted_list)
-        del sorted_list
+        """function that prints the list, but sorted
+            all the elements of the list is type int
+        """
+        arrange_list = sorted(self)
+        print(arrange_list)
+        del arrange_list
