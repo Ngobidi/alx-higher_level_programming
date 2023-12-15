6cb0b2c1ba3a928dbb834c716ffa5a847da849d6
Ngobidi <chibuzormaxwell@mail.com>
2023-12-14 22:38:13 -0800

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
---develop the database hbtn_0d_2 and the user user_0d_2
--- develop a database
+-- creates the database hbtn_0d_2 and the user user_0d_2
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- creates a user
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
