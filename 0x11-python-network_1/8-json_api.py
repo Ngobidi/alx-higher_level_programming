c60def4407a096aac80fd3ed44e06fccf84c99ce
Ngobidi <chibuzormaxwell@gmail.com>
2024-01-26 04:58:22 -0800

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""process a POST request to http://0.0.0.0:5000/search_user with a given letter.
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
 Usage: ./8-json_api.py <letter>
   - The letter is sent as the value of the variable `q`.
-  - print `q=""` when no letter is provided
+  - If no letter is provided, sends `q=""`.
 """
 import sys
 import requests
